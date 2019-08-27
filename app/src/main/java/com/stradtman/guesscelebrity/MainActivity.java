56302ea9b597ee838ac0d5959a6db0ddcf6572ee
Kevin <stradtkt22@gmail.com>
2019-08-26 22:45:52 -0400

    Added toast message

--- app/src/main/java/com/stradtman/guesscelebrity/MainActivity.java
+++ app/src/main/java/com/stradtman/guesscelebrity/MainActivity.java
@@ -7,7 +7,10 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
     int chosenCeleb = 0;
     int locationOfCorrectAnswer = 0;
     String[] answers = new String[4];
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    public void celebChosen(View view) {
+        if(view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Wrong it was " + celebNames.get(chosenCeleb), Toast.LENGTH_LONG).show();
+        }
+    }
 
     public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
         @Override
@@ -78,13 +93,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button0 = (Button) findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button2);
+        button2 = (Button) findViewById(R.id.button3);
+        button3 = (Button) findViewById(R.id.button4);
         imageView = (ImageView) findViewById(R.id.imageView);
         DownloadTask task = new DownloadTask();
         String result = null;
         try {
             result = task.execute("http://www.posh24.se/kandisar").get();
             String[] splitResult = result.split("<div class=\"sidebarContainer\"");
-            Pattern p = Pattern.compile("src=\"(.*?)\"");
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
             Matcher m = p.matcher(splitResult[0]);
 
             while(m.find()) {
@@ -119,6 +138,10 @@ public class MainActivity extends AppCompatActivity {
                     answers[i] = celebNames.get(incorrectAnswerLocation);
                 }
             }
+            button0.setText(answers[0]);
+            button1.setText(answers[1]);
+            button2.setText(answers[2]);
+            button3.setText(answers[3]);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
